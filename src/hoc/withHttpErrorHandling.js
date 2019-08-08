d5eb0263c2476d68f7ccfea1872e39e4f54bf821
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-08-08 09:08:24 +0200

    changed hoc for http errors handling

--- src/hoc/withHttpErrorHandling.js
+++ src/hoc/withHttpErrorHandling.js
@@ -4,32 +4,39 @@ import Modal from "../components/UI/Modal";
 
 const withHttpErrorHandling = (WrappedComponent, axios) => (
   class extends Component {
-    state = {
-      httpError: null
-    };
+    constructor(props) {
+      super(props);
 
-    componentDidMount() {
-      axios.interceptors.request.use(request => {
-        this.setState({
-          httpError: null
-        });
+      this.state = {
+        httpError: null
+      };
 
-        return request;
-      }, error => {
-        this.setState({
-          httpError: error
-        });
+      axios.interceptors.request.use(
+        request => {
+          this.setState({
+            httpError: null
+          });
 
-        return Promise.reject(error);
-      });
+          return request;
+        }, error => {
+          this.setState({
+            httpError: error
+          });
 
-      axios.interceptors.response.use(response => response, error => {
-        this.setState({
-          httpError: error
-        });
+          return Promise.reject(error);
+        }
+      );
 
-        return Promise.reject(error);
-      });
+      axios.interceptors.response.use(
+        response => response,
+        error => {
+          this.setState({
+            httpError: error
+          });
+
+          return Promise.reject(error);
+        }
+      );
     }
 
     onErrorModalClosed = () => {
